6382fbd037a7f110b1c6c6b5fb25865eb76910fb
Keberwein <kristian.eberwein@yahoo.com>
2016-04-01 10:29:49 -0400

    fixed zoo index

--- R/inflation.R
+++ R/inflation.R
@@ -2,7 +2,7 @@
 #' @description Returns a data frame that uses data from the Consumer Price Index (All Goods) to convert the value of a US Dollar [$1.00 USD] over time.
 #' @param base_year = A string or integer argument to represent the base year that you would like dollar values converted to. For example, if you want to see the value of a 2007 dollar in 2015, you would select 2015 as a base year and find 2007 in the table.
 #' @keywords bls api economics cpi unemployment inflation
-#' @import xts dplyr
+#' @import zoo xts
 #' @export inflation_adjust
 #' @examples
 #' 
@@ -54,7 +54,7 @@ inflation_adjust <- function(base_year=NA){
         #cf <- round(cf, 2)
         dat <- merge(avg.cpi, cf)
         # Xts object to data frame
-        dat <- data.frame(date=index(dat), coredata(dat))
+        dat <- data.frame(date=zoo:::index(dat), coredata(dat))
         dat$base.year <- as.character(base_year)
         dat$pct_increase <- (1-dat$adj_value) * -100
         # Round dollar amounts
