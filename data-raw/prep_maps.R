4a4004b71437a13146bb51f2a933c772bedd8ab5
Keberwein <kristian.eberwein@yahoo.com>
2016-06-11 13:38:17 -0400

    edits to map prep

--- data-raw/prep_maps.R
+++ data-raw/prep_maps.R
@@ -1,5 +1,5 @@
 # This function removes a lot of the US territories from the shapefile.
-# It also re-locates Alaska and Hawaii
+# It also re-locates ak and hawi
 
 library(sp)
 library(ggplot2)
@@ -11,38 +11,37 @@ library(maptools)
 # https://www.census.gov/geo/maps-data/data/cbf/cbf_counties.html
 county <- readOGR(dsn = "cb_2015_us_county_5m", layer = "cb_2015_us_county_5m")
 
-# convert it to Albers equal area
-us_aea <- spTransform(county, CRS("+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 
+# convert it to  equal area
+us.map <- spTransform(county, CRS("+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 
                                   +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"))
-us_aea@data$id <- rownames(us_aea@data)
+us.map@data$id <- rownames(us.map@data)
 
-# extract, then rotate, shrink & move alaska (and reset projection)
-# need to use state IDs via # https://www.census.gov/geo/reference/ansi_statetables.html
-alaska <- us_aea[us_aea$STATEFP=="02",]
-alaska <- elide(alaska, rotate=-50)
-alaska <- elide(alaska, scale=max(apply(bbox(alaska), 1, diff)) / 2.3)
-alaska <- elide(alaska, shift=c(-2100000, -2500000))
-proj4string(alaska) <- proj4string(us_aea)
+# Rotate and shrink ak.
+ak <- us.map[us.map$STATEFP=="02",]
+ak <- elide(ak, rotate=-50)
+ak <- elide(ak, scale=max(apply(bbox(ak), 1, diff)) / 2.3)
+ak <- elide(ak, shift=c(-2100000, -2500000))
+proj4string(ak) <- proj4string(us.map)
 
-# extract, then rotate & shift hawaii
-hawaii <- us_aea[us_aea$STATEFP=="15",]
-hawaii <- elide(hawaii, rotate=-35)
-hawaii <- elide(hawaii, shift=c(5400000, -1400000))
-proj4string(hawaii) <- proj4string(us_aea)
+# Rotate and Shift hawi
+hawi <- us.map[us.map$STATEFP=="15",]
+hawi <- elide(hawi, rotate=-35)
+hawi <- elide(hawi, shift=c(5400000, -1400000))
+proj4string(hawi) <- proj4string(us.map)
 
 # Also remove Puerto Rico (72), Guam (66), Virgin Islands (78), American Samoa (60) Mariana Islands (69)
 # Micronesia (64), Marshall Islands (68), Palau (70), Minor Islands (74)
-us_aea <- us_aea[!us_aea$STATEFP %in% c("02", "15", "72", "66", "78", "60", "69",
+us.map <- us.map[!us.map$STATEFP %in% c("02", "15", "72", "66", "78", "60", "69",
                                         "64", "68", "70", "74"),]
 # Make sure other outling islands are removed.
-us_aea <- us_aea[!us_aea$STATEFP %in% c("81", "84", "86", "87", "89", "71", "76",
+us.map <- us.map[!us.map$STATEFP %in% c("81", "84", "86", "87", "89", "71", "76",
                                         "95", "79"),]
-us_aea <- rbind(us_aea, alaska, hawaii)
+us.map <- rbind(us.map, ak, hawi)
 
 # Projuce map
-county_map <- fortify(us_aea, region="GEOID")
+county_map <- fortify(us.map, region="GEOID")
 # Remove helper data and save file. Be sure to remove .Randdom.seed if exists.
-rm(alaska, county, hawaii, us_aea)
+rm(ak, county, hawi, us.map)
 rm(.Random.seed)
 # Use devtools to save data set.
 devtools::use_data(county_map, overwrite = TRUE)
@@ -56,37 +55,37 @@ rm(county_map)
 state <- readOGR(dsn = "cb_2015_us_state_5m", layer = "cb_2015_us_state_5m")
 
 # convert it to Albers equal area
-us_aea <- spTransform(state, CRS("+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 
+us.map <- spTransform(state, CRS("+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 
                                   +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"))
-us_aea@data$id <- rownames(us_aea@data)
+us.map@data$id <- rownames(us.map@data)
 
-# extract, then rotate, shrink & move alaska (and reset projection)
-# need to use state IDs via # https://www.census.gov/geo/reference/ansi_statetables.html
-alaska <- us_aea[us_aea$STATEFP=="02",]
-alaska <- elide(alaska, rotate=-50)
-alaska <- elide(alaska, scale=max(apply(bbox(alaska), 1, diff)) / 2.3)
-alaska <- elide(alaska, shift=c(-2100000, -2500000))
-proj4string(alaska) <- proj4string(us_aea)
+# Extract Alaska; shrink and rotate.
+ak <- us.map[us.map$STATEFP=="02",]
+ak <- elide(ak, rotate=-50)
+ak <- elide(ak, scale=max(apply(bbox(ak), 1, diff)) / 2.3)
+ak <- elide(ak, shift=c(-2100000, -2500000))
+proj4string(ak) <- proj4string(us.map)
 
-# extract, then rotate & shift hawaii
-hawaii <- us_aea[us_aea$STATEFP=="15",]
-hawaii <- elide(hawaii, rotate=-35)
-hawaii <- elide(hawaii, shift=c(5400000, -1400000))
-proj4string(hawaii) <- proj4string(us_aea)
+# Extract Hawaii, shift and rotate.
+hawi <- us.map[us.map$STATEFP=="15",]
+hawi <- elide(hawi, rotate=-35)
+hawi <- elide(hawi, shift=c(5400000, -1400000))
+proj4string(hawi) <- proj4string(us.map)
 
-#Also remove Puerto Rico (72), Guam (66), Virgin Islands (78), American Samoa (60) Mariana Islands (69)
-#Micronesia (64), Marshall Islands (68), Palau (70), Minor Islands (74)
-us_aea <- us_aea[!us_aea$STATEFP %in% c("02", "15", "72", "66", "78", "60", "69",
+# Remove Alaska and Hawaii from map
+# Also remove Puerto Rico (72), Guam (66), Virgin Islands (78), American Samoa (60) Mariana Islands (69)
+# Micronesia (64), Marshall Islands (68), Palau (70), Minor Islands (74)
+us.map <- us.map[!us.map$STATEFP %in% c("02", "15", "72", "66", "78", "60", "69",
                                         "64", "68", "70", "74"),]
 #Make sure other outling islands are removed.
-us_aea <- us_aea[!us_aea$STATEFP %in% c("81", "84", "86", "87", "89", "71", "76",
+us.map <- us.map[!us.map$STATEFP %in% c("81", "84", "86", "87", "89", "71", "76",
                                         "95", "79"),]
-us_aea <- rbind(us_aea, alaska, hawaii)
+us.map <- rbind(us.map, ak, hawi)
 
 #Projuce map
-state_map <- fortify(us_aea, region="GEOID")
+state_map <- fortify(us.map, region="GEOID")
 # Remove helper data and save file. Be sure to remove .Randdom.seed if exists.
-rm(alaska, state, hawaii, us_aea)
+rm(ak, state, hawi, us.map)
 rm(.Random.seed)
 # Use devtools to save data set.
 devtools::use_data(state_map, overwrite = TRUE)
