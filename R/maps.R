89c6f8955fb5ff8fabb39e4a4ca9990b91c79f1a
Keberwein <kristian.eberwein@yahoo.com>
2016-06-16 11:03:15 -0400

    added libstitle arg to map funcs

--- R/maps.R
+++ R/maps.R
@@ -11,18 +11,19 @@
 #' @export bls_map_county
 #' @param map_data Dataframe to be used as the map's measures. Usually a result of function calls format_county_data or format_state_data, but other dataframes, which include FIPS codes may be used as well.
 #' @param fill_rate Column name from the dataframe that you want to use as a fill value.
+#' @param labtitle The main title label for your map passed as a string. The default is no title
 #' @examples
 #' 
 #' ## Not run:
 #' df <- format_county_data()
-#' bls_gg <- bls_map_county(map_data = df, fill_rate = "unemp_rate")
+#' bls_gg <- bls_map_county(map_data = df, fill_rate = "unemp_rate", labtitle = "Unemployment Rate")
 #' bls_gg
 #' 
 #' ## End (Not run)
 #'
 #'
 
-bls_map_county <- function(map_data, fill_rate){
+bls_map_county <- function(map_data, fill_rate, labtitle=NULL){
 #Maps by County
 #Load pre-formatted map for ggplot.
 map = county_map
@@ -34,9 +35,9 @@ ggplot() +
              fill="#ffffff", color="#0e0e0e", size=0.15) +
     geom_map(data=map_data, map=map, aes_string(map_id="fips", fill=fill_rate),
              color="#0e0e0e", size=0.15) +
-    scale_fill_gradientn(colors = terrain.colors(9)) +
+    scale_fill_gradientn(colors = c("green", "red")) +
     coord_equal() +
-    labs(title=fill_rate) + 
+    labs(title=labtitle) + 
     theme(axis.line=element_blank(),
           axis.text.x=element_blank(),
           axis.text.y=element_blank(),
@@ -61,9 +62,10 @@ ggplot() +
 #' @keywords bls api economics unemployment map geo geography
 #' @import ggplot2
 #' @import grDevices
-#' @export bls_map_county_state
+#' @export bls_map_state
 #' @param map_data Dataframe to be used as the map's measures. Usually a result of function calls format_county_data or format_state_data, but other dataframes, which include FIPS codes may be used as well.
 #' @param fill_rate Column name from the dataframe that you want to use as a fill value.
+#' @param labtitle The main title label for your map passed as a string. The default is no title
 #' @examples
 #' 
 #' ## Not run:
@@ -73,14 +75,14 @@ ggplot() +
 #' recent <- max(df$month)
 #' df <- df[ which(df$month==recent), ]
 #' 
-#' bls_gg <- bls_map_county_state(map_data = df, fill_rate = "unemployed_pct")
+#' bls_gg <- bls_map_state(map_data = df, fill_rate = "unemp_rate", labtitle = "Unemployment Rate")
 #' bls_gg
 #' 
 #' ## End (Not run)
 #'
 #'
 
-bls_map_county_state <- function(map_data, fill_rate){
+bls_map_state <- function(map_data, fill_rate, labtitle=NULL){
     #Maps by County
     #Load pre-formatted map for ggplot.
     map = state_map
@@ -92,7 +94,7 @@ bls_map_county_state <- function(map_data, fill_rate){
                  fill="#ffffff", color="#0e0e0e", size=0.15) +
         geom_map(data=map_data, map=map, aes_string(map_id="fips_state", fill=fill_rate),
                  color="#0e0e0e", size=0.15) +
-        scale_fill_gradientn(colors = topo.colors(20)) +
+        scale_fill_gradientn(colors = c("green", "red")) +
         coord_equal() +
         labs(title=fill_rate) + 
         theme(axis.line=element_blank(),
