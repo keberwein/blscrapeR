c557abc322bb09a32572f92b2b99404cd399ab41
Keberwein <kristian.eberwein@yahoo.com>
2016-06-27 18:50:32 -0400

    merge beta

--- R/get_bls_state.R
+++ R/get_bls_state.R
@@ -1,4 +1,3 @@
-
 #
 #' @title Function that returns county-level labor statistics for the last three months.
 #' @description Helper function to download and format state employment data.
@@ -78,15 +77,7 @@ get_bls_state <- function(seasonality = NA){
         df <- bind_rows(lapply(state.name, bls_state_data, seasonality))
         
     }
-    if (current=TRUE){
-        # Subset data frame to most recent month.
-        recent <- max(df$month)
-        df <- df[ which(df$month==recent), ]
-    }
-    
     state_fips<-state_fips
     df <- left_join(df, state_fips, by="state")
     return(df)
 }
-
-
