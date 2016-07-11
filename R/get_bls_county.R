1e7916bae087a5a5432d55d0027af23f2354ce9e
Keberwein <kristian.eberwein@yahoo.com>
2016-07-11 17:03:32 -0400

    added error handling

--- R/get_bls_county.R
+++ R/get_bls_county.R
@@ -4,7 +4,8 @@
 #' @importFrom stats na.omit
 #' @export get_bls_county
 #'
-get_bls_county <- function(){
+get_bls_county <- function(date_mth = NULL){
+    date_mth <- as.Date(paste('01', date_mth), format = '%d %b %Y')
     temp<-tempfile()
     download.file("http://www.bls.gov/lau/laucntycur14.txt", temp)
     countyemp<-read.csv(temp,
@@ -17,20 +18,43 @@ get_bls_county <- function(){
     colnames(countyemp) <- c("area_code", "fips_state", "fips_county", "area_title", "period",
                              "labor_force", "employed", "unemployed", "unemployed_rate")
     unlink(temp)
-    #Get rid of empty rows at the bottom.
+    # Get rid of empty rows at the bottom.
     countyemp <- na.omit(countyemp)
-    #Set period to proper date format.
-    countyemp$period <- as.Date(paste("01-", countyemp$period, sep = ""), format = "%d-%b-%y")
-    #Subset data frame to selected month.
-    recent <- max(countyemp$period)
-    countyemp <- countyemp[ which(countyemp$period==recent), ]
     
-    #Correct column data fromats
+    # Set period to proper date format.
+    countyemp$period <- as.Date(paste("01-", countyemp$period, sep = ""), format = "%d-%b-%y")
+    
+    # Check to see if users date exists
+    dt_exist <- any(grepl(date_mth, countyemp$period))
+    if(dt_exist==FALSE){
+        message("Are you sure that month is published? Please check the BLS release schedule.")
+        if(date_mth>Sys.Date()-54){
+            stop(
+            message("County-wide statistics are usually published on the third Friday of each month for the previous month."))
+        }
+        if(date_mth<Sys.Date()-360){
+            stop(
+                message("This data set only goes back one year. Make sure your date is correct.")
+            )
+        }
+    }
+    
+   if(!is.null(date_mth)){
+    # Subset data frame to selected month.
+    countyemp <- subset(countyemp, period==date_mth)
+   }
+    else{
+        current <- max(countyemp$period)
+        countyemp <- countyemp[ which(countyemp$period==current), ]
+        
+    }
+    
+    # Correct column data fromats
     countyemp$unemployed <- as.numeric(gsub(",", "", as.character(countyemp$unemployed)))
     countyemp$employed <- as.numeric(gsub(",", "", as.character(countyemp$employed)))
     countyemp$labor_force <- as.numeric(gsub(",", "", as.character(countyemp$labor_force)))
     
-    #Get the FIPS code: Have to add leading zeros to any single digit number and combine them.
+    # Get the FIPS code: Have to add leading zeros to any single digit number and combine them.
     countyemp$fips_county <- formatC(countyemp$fips_county, width = 3, format = "d", flag = "0")
     countyemp$fips_state <- formatC(countyemp$fips_state, width = 2, format = "d", flag = "0")
     countyemp$fips=paste(countyemp$fips_state,countyemp$fips_county,sep="")
