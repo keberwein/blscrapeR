ec3703603c07fdf7f286984352f0a67c0c191fc9
Keberwein <kristian.eberwein@yahoo.com>
2017-07-19 10:28:00 -0400

    added map_bls function

--- R/zzz.R
+++ R/zzz.R
@@ -8,10 +8,17 @@
 #' @import ggplot2
 #' @import grDevices
 #' @import utils
-#' @export bls_map_county
+#' @import tigris
+#' @importFrom maptools elide
+#' @importFrom broom tidy
+#' @importFrom sp spTransform bbox proj4string
+#' @import rgdal
+#' @export map_bls
 #' @param map_data Dataframe to be used as the map's measures. Usually a result of 
 #' calls to the \code{get_bls_county()} or \code{get_bls_state()} functions, but other dataframes, 
 #' which include FIPS codes may be used as well.
+#' @param fips_year The year of the available shape files that provides the closest match to your data. Current options are 2010-2016.
+#' The 2016 data is the default and fits the FIPS codes for curent data.
 #' @param fill_rate Column name from the dataframe that you want to use as a fill value, in quotes. NOTE: This argument is mandatory!
 #' @param stateName Optional argument if you only want to map a single state or a group of selected states. The argument
 #' accepts state full state names in quotes.
@@ -28,7 +35,7 @@
 #' df <- get_bls_county()
 #' 
 #' # Map the unemployment rate by county.
-#' bls_gg <- bls_map_county(map_data = df, fill_rate = "unemployed_rate", 
+#' bls_gg <- map_bls(map_data = df, fill_rate = "unemployed_rate", 
 #'                  labtitle = "Unemployment Rate")
 #' bls_gg
 #' 
@@ -37,7 +44,7 @@
 #' 
 #' df <- get_bls_county(stateName = c("Florida", "Alabama"))
 #' 
-#' bls_gg <- bls_map_county(map_data=df, fill_rate = "unemployed_rate", 
+#' bls_gg <- map_bls(map_data=df, fill_rate = "unemployed_rate", 
 #' stateName = c("Florida", "Alabama"))
 #' 
 #' bls_gg
@@ -45,25 +52,67 @@
 #'
 #'
 
-bls_map_county <- function(map_data, fill_rate=NULL, labtitle=NULL, stateName=NULL, projection=NULL, lowFill="green", highFill="red"){
-    if (is.null(fill_rate)){
-        stop(message("Please specify a fill_rate in double quotes. What colunm in your data frame do you want to map?"))
-    }
+map_bls <- function(map_data, fips_year=2016, fill_rate=NULL, labtitle=NULL, stateName=NULL, projection=NULL, lowFill="green", highFill="red"){
     # Set some dummy variables. This keeps CRAN check happy.
-    map=long=lat=id=group=county_map_data=NULL
-    # Load pre-formatted map for ggplot.
+    map=long=lat=id=group=county_map_data=CRS='proj4string<-'=NULL
+    # Check to make sure all requred variables are there.
+    if (is.null(fill_rate)){
+        warning("Please specify a fill_rate in double quotes. What colunm in your data frame do you want to map?")
+    }
+    if (is.null(map_data())){
+        warning("Please supply a data frame containing variables to be mapped. For help, see the get_bls_county() or get_bls_state() functions.")
+    }
+
+    # Load spatial data frame.
+    county <- tigris::counties(cb = TRUE, resolution = "20m", fips_year)
+    
+    # Format spatial data fram to specified projection.
+    # User Mercator projection for states unless the user overrides.
+
     if (is.null(projection)){
-        map <- blscrapeR::county_map_data
+        map <- spTransform(county, CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0
+                                    +x_0=0.0 +y_0=0 +k=1.0 +units=m +no_defs"))
+        map@data$id <- rownames(map@data)
     }else{
         if (tolower(projection)=="lambert"){
-            map <- blscrapeR::county_map_data
-        }
+            map <- spTransform(county, CRS("+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 
+                                  +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"))
+            map@data$id <- rownames(map@data)        }
         if (tolower(projection)=="mercator"){
-            map <- blscrapeR::county_map_merc
+            map <- spTransform(county, CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0
+                                    +x_0=0.0 +y_0=0 +k=1.0 +units=m +no_defs"))
+            map@data$id <- rownames(map@data)
         }else{
             message("Supported projections are Lambert and Mercator. A null projection argument returns Mercator for this function.")
         }
     }
+    
+    # Rotate and shrink ak.
+    ak <- map[map$STATEFP=="02",]
+    ak <- elide(ak, rotate=-50)
+    ak <- elide(ak, scale=max(apply(bbox(ak), 1, diff)) / 2.3)
+    ak <- elide(ak, shift=c(-2100000, -2500000))
+    proj4string(ak) <- proj4string(map)
+    
+    # Rotate and Shift hawi
+    hawi <- map[map$STATEFP=="15",]
+    hawi <- elide(hawi, rotate=-35)
+    hawi <- elide(hawi, shift=c(5400000, -1400000))
+    proj4string(hawi) <- proj4string(map)
+    
+    # Also remove Puerto Rico (72), Guam (66), Virgin Islands (78), American Samoa (60) Mariana Islands (69)
+    # Micronesia (64), Marshall Islands (68), Palau (70), Minor Islands (74)
+    map <- map[!map$STATEFP %in% c("02", "15", "72", "66", "78", "60", "69",
+                                            "64", "68", "70", "74"),]
+    # Make sure other outling islands are removed.
+    map <- map[!map$STATEFP %in% c("81", "84", "86", "87", "89", "71", "76",
+                                            "95", "79"),]
+    map <- rbind(map, ak, hawi)
+    
+    # Projuce map
+    map <- broom::tidy(map, region="GEOID")
+    # Remove helper data and save file. Be sure to remove .Randdom.seed if exists.
+    rm(ak, county, hawi)
     
     # Unemployment statistics by county: Get and process data.
     # Check to see if user selected specific state(s).
@@ -75,19 +124,7 @@ bls_map_county <- function(map_data, fill_rate=NULL, labtitle=NULL, stateName=NU
         if(any(state_check==FALSE)){
             stop(message("Please make sure you state names are spelled correctly using full state names."))
         }
-        # User Mercator projection for states unless the user overrides.
-        if (is.null(projection)){
-            map <- blscrapeR::county_map_merc
-        }else{
-            if (tolower(projection)=="lambert"){
-                map <- blscrapeR::county_map_data
-            }
-            if (tolower(projection)=="mercator"){
-                map <- blscrapeR::county_map_merc
-            }else{
-                message("Supported projections are Lambert and Mercator. A null projection argument returns Mercator for this function.")
-            }
-        }
+
         # If state list is valid. Grab State FIPS codes from internal data set and subset map.
         # Add state_id to map frame
         map$state_id <- substr(map$id, 1,2)
@@ -97,6 +134,9 @@ bls_map_county <- function(map_data, fill_rate=NULL, labtitle=NULL, stateName=NU
         map <- map[(map$state_id %in% state_selection),]
     }
     # Plot
+    # 
+    # Working up to here---Need to figure out scale_fill_gradient in ggplot. Though this is working in the production version.
+    # TODO: Center lab title.
     ggplot2::ggplot() + 
         ggplot2::geom_map(data=map, map=map, ggplot2::aes(x=long, y=lat, map_id=id, group=group)) +
         ggplot2::geom_map(data=map_data, map=map, ggplot2::aes_string(map_id="fips", fill=fill_rate), color="#0e0e0e", size=0.25) +
